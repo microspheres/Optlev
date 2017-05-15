918b49426b4b32186e4867bae5c56dcb60becc9e
sumitaghosh <sumita.ghosh@yale.edu>
2017-05-15 16:36:24 -0400

    Fix ACandDC

--- scripts/VoltagevsAmplitude.py
+++ scripts/VoltagevsAmplitude.py
@@ -1,18 +1,13 @@
-import numpy, h5py, matplotlib
-import matplotlib.pyplot as plt
-import os
-import scipy.signal as sp
-import numpy as np
 import numpy, h5py, matplotlib, os, glob
 
-### The functions "saveACfile" and "saveACandDCfile" take in a path string (as below)
-### then find the file_list and save the values from that list
-### so you don't actually have to do anything other than type in "saveACfile(path)".
-#path = r"/data/20170511/bead2_15um_QWP/new_sensor_feedback/charge7_piezo_56.9_74.9_75.4"
+"""Both "saveACfile" and "saveACandDCfile" take in a path string such as
+   saveACfile(r"/data/20170511/bead2_15um_QWP/new_sensor_feedback/charge7_piezo_56.9_74.9_75.4")
+   which find the file list from the path and get all the voltage and frequency values from that
+   which means that you don't have to do anything other than use the "save_" functions"""
+
 conversion = 4.1e-13
-Fs = 10e3  ## this is ignored with HDF5 files
+Fs = 10e3
 NFFT = 2 ** 13
-#file_list = glob.glob(path+"/*.h5")
 
 def getdata(fname):
     ## guess at file type from extension
@@ -27,7 +22,8 @@ def getdata(fname):
         dat = numpy.loadtxt(fname, skiprows=5, usecols=[2, 3, 4, 5, 6])
     xpsd, freqs = matplotlib.mlab.psd(dat[:, 0] - numpy.mean(dat[:, 0]), Fs=Fs, NFFT=NFFT)
     drive, freqs = matplotlib.mlab.psd(dat[:, 5] - numpy.mean(dat[:, 5]), Fs=Fs, NFFT=NFFT)
-    return [freqs, xpsd, drive]
+    #return [freqs, xpsd, drive]
+    return [xpsd, drive]
 
 def getACAmplitudeGraphs(file_list):
     """output AC voltages and corresponding amplitudes at both omega and 2 omega for a DC voltage of 0"""
@@ -48,12 +44,12 @@ def getACAmplitudeGraphs(file_list):
         if ACvoltage in x:
             if DCvoltage == 0:
                 #voltageCount[ACvoltage] += 1
-                x[ACvoltage] += numpy.sqrt(a[1])
-                dx[ACvoltage] += numpy.sqrt(a[2])
+                x[ACvoltage] += numpy.sqrt(a[0])
+                dx[ACvoltage] += numpy.sqrt(a[1])
         else:
             #voltageCount[ACvoltage] = 1
-            x[ACvoltage] = numpy.sqrt(a[1])
-            dx[ACvoltage] = numpy.sqrt(a[2])
+            x[ACvoltage] = numpy.sqrt(a[0])
+            dx[ACvoltage] = numpy.sqrt(a[1])
     ACvoltages = x.keys()
     N1 = len(ACvoltages)
     DCvoltages = [0] * N1
@@ -69,12 +65,12 @@ def getACAmplitudeGraphs(file_list):
     return ACvoltages, omegaAmplitudes, twoOmegaAmplitudes, DCvoltages
 
 def getACandDCAmplitudeGraphs(file_list):
-    """output AC and non-zero DC voltages and corresponding amplitudes at both omega and 2 omega"""
+    """output AC voltages and corresponding amplitudes at both omega and 2 omega for a DC voltage of 0"""
     N = len(file_list)
     constant = conversion/N
     x = {} # input only numpy arrays as values
     dx = {} # input only numpy arrays as values
-    #voltageCount = {} # input integers that count how many times an AC/DC pair has shown up
+    #voltageCount = {} # input integers that count how many times an AC voltage value has shown up
     for index in range(N):
         f = file_list[index]
         a = getdata(f)
@@ -84,27 +80,23 @@ def getACandDCAmplitudeGraphs(file_list):
         l = f.rfind("Hz") + 2
         ACvoltage = float(f[i:k])/1000.
         DCvoltage = float(f[l:j])/1000.
-        voltages = (ACvoltage, DCvoltage)
-        if voltages in x:
+        if ACvoltage in x:
             if DCvoltage != 0:
-                #voltageCount[voltages] += 1
-                x[voltages] += numpy.sqrt(a[1])
-                dx[voltages] += numpy.sqrt(a[2])
+                #voltageCount[ACvoltage] += 1
+                x[ACvoltage] += numpy.sqrt(a[0])
+                dx[ACvoltage] += numpy.sqrt(a[1])
         else:
-            #voltageCount[voltages] = 1
-            x[voltages] = numpy.sqrt(a[1])
-            dx[voltages] = numpy.sqrt(a[2])
-    voltageValues = x.keys()
-    N1 = len(voltageValues)
-    ACvoltages = range(N1)
+            #voltageCount[ACvoltage] = 1
+            x[ACvoltage] = numpy.sqrt(a[0])
+            dx[ACvoltage] = numpy.sqrt(a[1])
+    ACvoltages = x.keys()
+    N1 = len(ACvoltages)
+    DCvoltages = [0] * N1
     omegaAmplitudes = range(N1)
     twoOmegaAmplitudes = range(N1)
-    DCvoltages = range(N1)
     """Now insert the amplitude for the requisite frequencies"""
     for index in range(N1):
-        volt = voltageValues[index]
-        ACvoltages[index] = volt[0]
-        DCvoltages[index] = volt[1]
+        volt = ACvoltages[index]
         i = numpy.argmax(dx[volt])
         psd = x[volt]
         omegaAmplitudes[index] = constant*psd[i]
@@ -114,11 +106,11 @@ def getACandDCAmplitudeGraphs(file_list):
 def saveACfile(path):
     file_list = glob.glob(path+"/*.h5")
     ACvoltages, omegaAmplitudes, twoOmegaAmplitudes, DCvoltages = getACAmplitudeGraphs(file_list)
-    np.savetxt(path+'/ACamplitudes.txt', (ACvoltages, omegaAmplitudes, twoOmegaAmplitudes, DCvoltages))
+    numpy.savetxt(path+'/ACamplitudes.txt', (ACvoltages, omegaAmplitudes, twoOmegaAmplitudes, DCvoltages))
     return
 
 def saveACandDCfile(path):
     file_list = glob.glob(path+"/*.h5")
     ACvoltages, omegaAmplitudes, twoOmegaAmplitudes, DCvoltages = getACandDCAmplitudeGraphs(file_list)
-    np.savetxt(path+'/ACandDCamplitudes.txt', (ACvoltages, omegaAmplitudes, twoOmegaAmplitudes, DCvoltages))
+    numpy.savetxt(path+'/ACandDCamplitudes.txt', (ACvoltages, omegaAmplitudes, twoOmegaAmplitudes, DCvoltages))
     return
