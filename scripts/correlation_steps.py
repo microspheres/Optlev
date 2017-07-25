31a906d8b948fa306900aad0636e8d82db5b398b
sumitaghosh <sumita.ghosh@yale.edu>
2017-07-25 15:13:05 -0400

    Add plotGaussFit2 to correlation_steps

--- scripts/correlation_steps.py
+++ scripts/correlation_steps.py
@@ -143,6 +143,21 @@ def plotGaussFit(corr):
     plt.show()
 
 
+def plotGaussFit2(data):
+    # Fit a normal distribution to the data:
+    mu, std = norm.fit(data)
+    # Plot the histogram.
+    plt.hist(data, bins=25, normed=True, alpha=0.6, color='g')
+    # Plot the PDF.
+    xmin, xmax = plt.xlim()
+    x = np.linspace(xmin, xmax, 100)
+    p = norm.pdf(x, mu, std)
+    plt.plot(x, p, 'k', linewidth=2)
+    title = "Fit results: mu = %.2f,  std = %.2f" % (mu, std)
+    plt.title(title)
+    plt.show()
+
+
 if use_as_script:
     t, dc, corr = formData(measurement_path, calibration_path)
     print "average correlation is ", float(sum(corr))/float(len(corr))
